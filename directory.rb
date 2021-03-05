fb614292ef860291ace0438eff420970d19de250
Callum Ahern <callumahern@gmail.com>
2021-03-05 12:44:40 +0000

    eachIterationIncluded

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
 #list of students
+# student list in array
 
+students = [
+     "Dr. Hannibal Lecter",
+     "Darth Vader",
+     "Nursh Ratched",
+     "Michael Corleone",
+     "Alex DeLarge",
+     "The Wicked Witch of the West",
+     "Terminator",
+     "Freddy Krueger",
+     "The Joker",
+     "Joffrey Baratheon",
+     "Norman Bates"
+]
 puts "The students of Villans Academy"
-puts "---------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nursh Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts "-------"
 
-# number of students
+# iteration method, students.each calls each the array collectively - do |var| puts var - this prints the array out
+students.each do |var|
+    puts var
+end
 
-puts "Overall we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
