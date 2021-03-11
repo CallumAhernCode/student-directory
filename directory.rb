3b70117361883e4fca36593c7e609b5372811773
Callum Ahern <callumahern@gmail.com>
2021-03-11 11:49:01 +0000

    added method

--- directory.rb
+++ directory.rb
@@ -2,24 +2,35 @@
 # student list in array
 
 students = [
-     "Dr. Hannibal Lecter",
-     "Darth Vader",
-     "Nursh Ratched",
-     "Michael Corleone",
-     "Alex DeLarge",
-     "The Wicked Witch of the West",
-     "Terminator",
-     "Freddy Krueger",
-     "The Joker",
-     "Joffrey Baratheon",
-     "Norman Bates"
+    ["Dr. Hannibal Lecter", :november],
+    ["Darth Vader", :november],
+    ["Nursh Ratched", :november],
+    ["Michael Corleone", :november],
+    ["Alex DeLarge", :november],
+    ["The Wicked Witch of the West", :november],
+    ["Terminator", :november],
+    ["Freddy Krueger", :november],
+    ["The Joker", :november],
+    ["Joffrey Baratheon", :november],
+    ["Norman Bates", :november] 
 ]
-puts "The students of Villans Academy"
-puts "-------"
 
-# iteration method, students.each calls each the array collectively - do |var| puts var - this prints the array out
-students.each do |var|
-    puts var
+def header
+    puts "The students of Villans Academy"
+    puts "-------"
+end
+def body(students) # iteration method, students.each calls each the array collectively - do |var| puts var - this prints the array out
+    students.each do |student|
+        puts "#{student[0]} (#{student[1]} cohort)"
+    end #end for do
+end # end for def 
+
+
+def footer(students)
+    puts "Overall, we have #{students.count} great students"
 end
 
-puts "Overall, we have #{students.count} great students"
+
+header
+body(students)
+footer(students)
